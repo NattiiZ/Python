6c74c173afcca145e39841806ca981c985038e00
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-09-09 11:18:55 +0700

    Add files via upload

---
+++ min_max.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter value number 1 : "))
+num2 = int(input("Enter value number 2 : "))
+num3 = int(input("Enter value number 3 : "))
+
+minValue = min(num1, num2, num3)
+maxValue = max(num1, num2, num3)
+
+print()
+print("Your enter number : ", num1, num2, num3)
+print("Maximun value = ", maxValue)
+print("Minimun value = ", minValue)
