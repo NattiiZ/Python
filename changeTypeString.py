ad0ab4b888d0044fe3c74fe6bf35c034e96bd693
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-09-09 11:47:14 +0700

    Add files via upload

---
+++ changeTypeString.py
@@ -0,0 +1,7 @@
+s = input("Enter string : ")
+
+print()
+print("Lower string =", s.lower())
+print("Upper string =", s.upper())
+print("Cappitalize sting =", s.capitalize())
+print("SwapCase string =", s.swapcase())
