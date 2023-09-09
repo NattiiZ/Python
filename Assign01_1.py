f2d4b5503a2b7bb0e73e89722cdc5e242af66f26
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-09-09 10:29:35 +0700

    Add files via upload

---
+++ Assign01_1.py
@@ -0,0 +1,12 @@
+myName = (input("Enter your name : "))
+mySurname = (input("Enter your surname : "))
+myAge = int(input("Enter your age : "))
+
+#calculator
+ageOn4 = myAge + 4
+
+print('\n')
+print("My name\' ",myName)
+print("My surname\'s ",mySurname)
+print("Now I\'m ",myAge,"years old.")
+print("I\'ll finish undergraduate in ",ageOn4,"year old.")
