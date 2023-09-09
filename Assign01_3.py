aa5bb53a0cf8ef04b049c15521547bf626f24f90
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-09-09 10:25:20 +0700

    Add files via upload

---
+++ Assign01_3.py
@@ -0,0 +1,10 @@
+fNum1 = float(input("Enter float number No.1 : "))
+fNum2 = float(input("Enter float number No.2 : "))
+fNum3 = float(input("Enter float number No.3 : "))
+
+#calculator
+toatal = fNum1+fNum2+fNum3
+
+print('\n')
+print("Float number No.1 =",fNum1,"\nFloat number No.2 =",fNum2,"\nFloat number No.3 =",fNum3)
+print("Toatal =",toatal)
