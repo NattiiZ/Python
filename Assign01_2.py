33556263223a8083bf40358cf4006afeab1cec6a
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-09-09 10:29:00 +0700

    Add files via upload

---
+++ Assign01_2.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter number No.1 : "))
+num2 = int(input("Enter number No.2 : "))
+num3 = int(input("Enter number No.3 : "))
+
+#calculator
+toatal = num1+num2+num3
+
+print('\n')
+print("number No.1 =",num1,"\nnumber No.2= ",num2,"\nnumber No.3 =",num3)
+print("Toatal =",toatal)
