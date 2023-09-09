a32032f6db0062fec042c903d2d9f4219c0e51a7
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-09-09 11:52:49 +0700

    Add files via upload

---
+++ index_name.py
@@ -0,0 +1,16 @@
+name = 'Nattawut'
+
+print("Length of name variable = ", len(name))
+print("Position 0 : ",name[0])
+print("Position 1 : ",name[1])
+print("Position 2 : ",name[2])
+
+print("Slicing 0:3 = ", name[0:3])
+print("Slicing 5:3 = ", name[5:9])
+print("Slicing 0:8 = ", name[0:])
+print("Slicing 0:3 = ", name[:3])
+print("Slicing all = ", name[:])
+print("Slicing -1:-4 = ", name[-1:-4])
+
+print(name[0::3])
+print(name[0::1])
