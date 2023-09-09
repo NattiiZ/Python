29cbcbf920ed3d87fbcff6b04bcb9d49f7ec1c26
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-09-09 12:21:20 +0700

    Add files via upload

---
+++ while_countStr.py
@@ -0,0 +1,10 @@
+messege = ''
+Max = 5
+count = 1
+while count <= Max:
+    Str = input(f'Enter string #{count} : ')
+    messege += Str + "\n"
+    count += 1
+
+print("\nPrint your string enter : ")
+print(messege)
