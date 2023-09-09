64fe8a586893c29469d6b65c46e6d08c78f380fe
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-09-09 12:04:58 +0700

    Add files via upload

---
+++ set_print_indent.py
@@ -0,0 +1,26 @@
+name = 'Python'
+
+print('|%10s|' %name)
+print('|%-10s|' %name)
+print('|%-10c|' %name[0])
+print('|%10c|' %name[0])
+
+
+num = 8
+
+print()
+print('|%d|' %num)
+print('|%3d|' %num)
+print('|%-3d|' %num)
+print('|%-03d|' %num)
+print('|%03d|' %num)
+
+
+gpa = 2.9850
+print()
+print('%d' %gpa)
+print('%f' %gpa)
+print('%.3f' %gpa)
+print('%4.2f' %gpa)
+print('%06.2f' %gpa)
+print('%6.2f' %gpa)
