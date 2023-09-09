a09989ba0cd3790a3a5d4e390177b27a99a45152
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-09-09 12:24:00 +0700

    Add files via upload

---
+++ whileStatement.py
@@ -0,0 +1,15 @@
+n = 1
+while n < 5:
+    print(n)
+    n += 1
+
+print()
+
+S = ''
+# while S != '0':
+#     S = input('Enter number : ')
+
+while True:
+    S = input('Enter number : ')
+    if S == '0':
+        break
