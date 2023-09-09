9950b5eb7355c21c130d37e18fd9e4feeb53aa5e
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-09-09 11:23:02 +0700

    Add files via upload

---
+++ summation.py
@@ -0,0 +1,11 @@
+Sum = 0
+
+Sum += int(input("Enter number 1 : "))
+Sum += int(input("Enter number 2 : "))
+Sum += int(input("Enter number 3 : "))
+Sum += int(input("Enter number 4 : "))
+
+Avg = Sum / 4
+print()
+print("Summation = ", Sum)
+print("Average = ", Avg)
