93b981ce76324659cda4c2dd8ed0718daaec3d4a
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-09-09 12:03:20 +0700

    Add files via upload

---
+++ repeat_triangle.py
@@ -0,0 +1,11 @@
+name = input("Enter your name : ")
+repeat = 5
+
+print()
+print(name * repeat)
+print("Show triangle")
+print(repeat * '*')
+print((repeat-1) * '*')
+print((repeat-2) * '*')
+print((repeat-3) * '*')
+print((repeat-4) * '*')
