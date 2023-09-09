3ee601ed10b9bb53d879dac13729d48010c81b57
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-09-09 12:17:13 +0700

    Add files via upload

---
+++ for_string.py
@@ -0,0 +1,19 @@
+s = 'Python'
+print(s[0])
+print()
+
+for c in s:
+    print(c)
+    print()
+
+
+for n in range(len(s)):
+    print(s[n])
+    print()
+
+
+num = '12345'
+summ = 0
+for n in num:
+    summ += int(n)
+    print(summ)
