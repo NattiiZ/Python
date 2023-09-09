d9679a359dba780f16afa923aa6ff1ca66a139e0
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-09-09 12:18:12 +0700

    Add files via upload

---
+++ forStatement.py
@@ -0,0 +1,32 @@
+# range(stop) ------------
+print('range(stop')
+
+for n in range(5):
+    print(n)
+    print()
+
+
+r = range(5)
+for n in r:
+    print(n)
+    print()
+
+
+# range(stop, start) ------------
+print('range(stop, start')
+
+for n in range(1, 6):
+    print(n)
+    print()
+
+
+# range(stop, start, step) ------------
+print('range(stop, start, step')
+
+for n in range(10, 20, 2):
+    print(n)
+    print()
+
+
+for n in range(20, 10, -2):
+    print(n)
