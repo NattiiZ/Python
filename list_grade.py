f1c65d736537a14423e8f57ab7b705d913a8955c
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-09-13 15:14:02 +0700

    Add files via upload

---
+++ list_grade.py
@@ -0,0 +1,24 @@
+def main():
+    loop = True
+    while loop:
+        grade = input('Enter grade (Q-Exit) : ')
+        grade = grade.upper()
+        if grade == 'Q':
+            loop = False
+        else:
+            value = checkGrade(grade)
+            print(f'Score value of {grade} is {value}\n')
+    
+    print('Exit Program')
+    print()
+
+
+def checkGrade(grade):
+    Grades = ['A', 'B', 'C', 'D', 'F']
+    Values = [4, 3, 2, 1, 0]
+    for i in range(len(Grades)):
+        if grade == Grades[i]:
+            return(Values[i])
+
+
+main()
