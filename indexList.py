0d081397ceda1011f76fc6d0a07dee7d44a9609f
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-09-13 15:13:40 +0700

    Add files via upload

---
+++ indexList.py
@@ -0,0 +1,21 @@
+E = ['Nat', 'IT', 20, 3.00]
+for i in range(4):
+    print(E[i])
+
+print()
+for i in E:
+    print(i)
+
+print()
+print(E[0].count('N'))
+
+
+print()
+Num = [1, 3, 5, 7, 9]
+for n in range(5):
+    print(Num[n])
+
+print()
+Colors = ['red', 'green', 'blue']
+for c in Colors:
+    print(c)
