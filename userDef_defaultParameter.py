b41bfc18ecfae49bd64fd17dc4c17ba383989de9
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-09-13 13:22:40 +0700

    Add files via upload

---
+++ userDef_defaultParameter.py
@@ -0,0 +1,14 @@
+def addValue(a=10, b=20):
+    return(a+b)
+
+print(addValue(30, 30))
+print(addValue(30))
+print(addValue())
+
+print()
+
+def addValue1(a, b=20):
+    return(a+b)
+
+print(addValue1(30, 30))
+print(addValue1(30))
