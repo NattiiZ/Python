de47949214f0b3ce2a2cfc0b070751abee9b7a30
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-09-13 16:02:04 +0700

    Add files via upload

---
+++ listFunction.py
@@ -0,0 +1,34 @@
+A = [1, 3, 5, 7, 9, 11]
+A.append(13)
+print(A)
+
+print()
+A.insert(2, 6)
+print(A)
+A.insert(0, 50)
+print(A)
+
+print()
+A.extend([20, 30, 40, 50])
+print(A)
+
+print()
+A.remove(50)
+print(A)
+
+print()
+print(A.pop())
+print(A)
+
+print()
+print(A.index(9))
+
+print()
+A.reverse()
+print(A)
+
+print()
+A.sort()
+print(A)
+A.reverse()
+print(A)
