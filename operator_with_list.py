2e6738a088e6e438112e520c2d2406d946a0e990
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-09-13 15:34:08 +0700

    Add files via upload

---
+++ operator_with_list.py
@@ -0,0 +1,28 @@
+A = [1, 3]
+B = [2, 4]
+
+C = A+B
+C += [5, 6, 7, 8]
+
+C[5] = 150
+
+print(C)
+
+print(3 in C)
+print(3 not in C)
+print(0 in C)
+print(0 not in C)
+
+
+print()
+
+D = [2]*4
+print(D)
+
+D = [2,]*4
+print(D)
+
+print(len(D))
+print(sum(D))
+print(max(C))
+print(min(C))
