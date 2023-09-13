61099f6e650692f84f97c2f725ed6e7821792d95
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-09-13 13:28:29 +0700

    Add files via upload

---
+++ userDef_void.py
@@ -0,0 +1,20 @@
+def sayHello():
+    print('Hello')
+
+sayHello()
+
+
+
+def sayMessage(text):
+    print(text)
+
+sayMessage("Nat")
+sayMessage(123456)
+
+
+
+def sayMessage(text1, text2):
+    print(text1, text2)
+
+name = "Nat"
+sayMessage(name, 123456)
